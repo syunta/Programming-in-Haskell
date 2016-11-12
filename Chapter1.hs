38e4d7fd8f19fc745d794684679ac297f073052a
syunta <kitagawa@timedia.co.jp>
2016-11-12 12:26:06 +0900

    Answer 1.1 - 1.4

---
+++ Chapter1.hs
@@ -0,0 +1,35 @@
+-- 1.
+{-
+ 
+正規順序の評価
+
+  double (double 2)
+= { 外側のdoubleを適用 }
+  double 2 + double 2
+= { + を適用 }
+  double 2 + double 2
+= { 一番目のdoubleを適用 }
+  (2 + 2) + double 2
+= { + を適用 }
+  4 + double 2
+= { doubleを適用 }
+  4 + (2 + 2)
+= { + を適用 }
+  8
+ -}
+
+-- 2.
+-- x + 0 = x
+
+-- 3.
+product' :: Num a => [a] -> a
+product' [] = 1
+product' (x:xs) = x * product' xs
+
+-- 4.
+qsort :: Ord a => [a] -> [a]
+qsort []     = []
+qsort (x:xs) = qsort larger ++ [x] ++ qsort smaller
+               where
+                 smaller = [a | a <- xs, a <= x]
+                 larger  = [b | b <- xs, b > x]
