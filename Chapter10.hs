b7c86f266006c277fd945067d35990d4c6eaf3bb
syunta <kitagawa@timedia.co.jp>
2016-11-24 16:02:39 +0900

    Answer 10.3 - 10.5

--- Chapter10.hs
+++ Chapter10.hs
@@ -1,5 +1,7 @@
 module Chapter10 where
 
+import Chapter4 (halve)
+
 data Nat = Zero | Succ Nat
            deriving (Show, Eq)
 
@@ -15,7 +17,7 @@ mult' m (Succ n) = add m (mult' m n)
 
 -- 2.
 data Tree = Leaf Int | Node Tree Int Tree
-            deriving Show
+            deriving (Show, Eq)
 
 -- 比較が一回で済むため効率が良い
 occurs :: Int -> Tree -> Bool
@@ -24,3 +26,74 @@ occurs m (Node l n r) = case compare m n of
                           EQ -> True
                           LT -> occurs m l
                           GT -> occurs m r
+
+-- 3.
+countleaves :: Tree -> Int
+countleaves (Leaf n) = 1
+countleaves (Node l _ r) = countleaves l + countleaves r
+
+balanced :: Tree -> Bool
+balanced (Leaf n) = False
+balanced (Node l _ r) = abs (countleaves l - countleaves r) <= 1
+
+-- 4.
+balance :: [Int] -> Tree
+balance xs  = case halve xs of
+                ([y],[])    -> Leaf y
+                ([],[y])    -> Leaf y
+                (ys,(z:zs)) -> Node (balance ys) z (balance zs)
+
+-- 5.
+
+-- Propositions
+
+data Prop = Const Bool
+          | Var Char
+          | Not Prop
+          | And Prop Prop
+          | Or Prop Prop
+          | IFF Prop Prop
+          | Imply Prop Prop
+
+-- Substitutions
+
+type Subst = Assoc Char Bool
+
+type Assoc k v = [(k,v)]
+
+find :: Eq k => k -> Assoc k v -> v
+find k t = head [v | (k',v) <- t, k == k']
+
+-- Tautology checker
+
+eval' :: Subst -> Prop -> Bool
+eval' _ (Const b)   = b
+eval' s (Var x)     = find x s
+eval' s (Not p)     = not (eval' s p)
+eval' s (And p q)   = eval' s p && eval' s q
+eval' s (Or p q)    = eval' s p || eval' s q
+eval' s (IFF p q)   = eval' s p == eval' s q
+eval' s (Imply p q) = eval' s p <= eval' s q
+
+vars :: Prop -> [Char]
+vars (Const _)   = []
+vars (Var x)     = [x]
+vars (Not p)     = vars p
+vars (And p q)   = vars p ++ vars q
+vars (Imply p q) = vars p ++ vars q
+
+bools :: Int -> [[Bool]]
+bools 0 = [[]]
+bools n = map (False:) bss ++ map (True:) bss
+          where bss = bools (n-1)
+
+rmdups :: Eq a => [a] -> [a]
+rmdups []     = []
+rmdups (x:xs) = x : filter (/= x) (rmdups xs)
+
+substs :: Prop -> [Subst]
+substs p = map (zip vs) (bools (length vs))
+           where vs = rmdups (vars p)
+
+isTaut :: Prop -> Bool
+isTaut p = and [eval' s p | s <- substs p]
--- Spec.hs
+++ Spec.hs
@@ -234,9 +234,10 @@ main = hspec $ do
   describe "Chapter10.1 mult" $ do
     it "calculates multiplication" $ do
       mult' (Succ (Succ (Succ Zero))) (Succ (Succ Zero)) `shouldBe` (Succ (Succ (Succ (Succ (Succ (Succ Zero))))))
+
+  let t = Node (Node (Leaf 1) 3 (Leaf 4)) 5 (Node (Leaf 6) 7 (Leaf 9))
   describe "Chapter10.2 occurs" $ do
     it "checks a tree whether contains a target element" $ do
-      let t = Node (Node (Leaf 1) 3 (Leaf 4)) 5 (Node (Leaf 6) 7 (Leaf 9))
       occurs 1 t `shouldBe` True
       occurs 3 t `shouldBe` True
       occurs 4 t `shouldBe` True
@@ -245,3 +246,29 @@ main = hspec $ do
       occurs 7 t `shouldBe` True
       occurs 9 t `shouldBe` True
       occurs 2 t `shouldBe` False
+  describe "Chapter10.3 countleaves" $ do
+    it "counts leaves of tree" $ do
+      countleaves t `shouldBe` 4
+  describe "Chapter10.3 balanced" $ do
+    it "checks a tree whether it is balanced" $ do
+      balanced t `shouldBe` True
+      balanced (Node (Node (Leaf 1) 2 (Node (Leaf 4) 3 (Leaf 5))) 6 (Leaf 8)) `shouldBe` False
+  describe "Chapter10.4 balance" $ do
+    it "generate a balanced tree from a list of interger" $ do
+      balance [1,3,4,5,6,7,9] `shouldBe` t
+      balanced (balance [1,3,4,5,6,7,9]) `shouldBe` True
+  describe "Chapter10.5 eval'" $ do
+    let t1 = [('A', True), ('B', True)]
+        t2 = [('A', True), ('B', False)]
+        t3 = [('A', False), ('B', True)]
+        t4 = [('A', False), ('B', False)]
+    it "supports disjunction" $ do
+      eval' t1 (Or (Var 'A') (Var 'B')) `shouldBe` True
+      eval' t2 (Or (Var 'A') (Var 'B')) `shouldBe` True
+      eval' t3 (Or (Var 'A') (Var 'B')) `shouldBe` True
+      eval' t4 (Or (Var 'A') (Var 'B')) `shouldBe` False
+    it "supports equivalence" $ do
+      eval' t1 (IFF (Var 'A') (Var 'B')) `shouldBe` True
+      eval' t2 (IFF (Var 'A') (Var 'B')) `shouldBe` False
+      eval' t3 (IFF (Var 'A') (Var 'B')) `shouldBe` False
+      eval' t4 (IFF (Var 'A') (Var 'B')) `shouldBe` True
